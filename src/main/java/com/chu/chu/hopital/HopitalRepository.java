7d23368d780524860381b80a49c9d20ae91e282a
Moncif elmouden <moncif.el-mouden@etu.u-pec.fr>
2023-03-05 21:25:15 +0100

    Repo entité

--- src/main/java/com/chu/chu/hopital/HopitalRepository.java
+++ src/main/java/com/chu/chu/hopital/HopitalRepository.java
@@ -5,4 +5,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HopitalRepository extends JpaRepository<Hopital,Long> {
+
 }
+// Le repository c'est celui qui va faire la liasion entre le code java et le sql avec la BD
+// plus de sql juste du java avec la JPA(vous en apprendrez au fur et à mesure)
+// ici on peut même creer des requetes specialiser, avec du java ;) exemple:
+//    Hopital getHopitalByNom(String Nom);
+//    List<Hopital> getHopitalByCapacite(Long cap);
+// des requetes pour selectionner les hopitaux par capacite et nom, on peut selectionner par
+// tous les attributs qu'on a dans l'entité
